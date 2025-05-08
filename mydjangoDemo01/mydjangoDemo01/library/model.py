9dd2fc30fdeeb57b4115b31a630f67380a8f7bf4
Zhiyu Tan <zhiyutan@ZhiyudeMacBook-Pro.local>
2025-05-08 21:13:25 +0800

    add files

---
+++ mydjangoDemo01/mydjangoDemo01/library/model.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+
+'''定义模型对象（其实就是数据库中的“表的模板”）'''
+'''django\db\models\fields\__init__.py中有所有的类型'''
+class UserInfo(models.Model):
+    # 定义属性 也就是  表中列的定义
+    UserID=models.BigIntegerField(primary_key=True)  
+    UserPassword=models.CharField(max_length=100,null=True)
+    Name=models.CharField(max_length=50,null=True)
+    Gender=models.BooleanField(null=True)
+    Email=models.EmailField(unique=True,null=True) #学生与邮箱必须一一对应
+    
